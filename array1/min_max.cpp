f54e9bdfa834907c69f5418eed55412e3f2fce23
Prakarsh4 <prakarshkumar819@gmail.com>
2026-06-08 13:19:21 +0530

    basic

---
+++ array1/min_max.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+ int main() {
+    int n, max, min;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    max = arr[0];
+    min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    cout << "Maximum element: " << max << endl;
+    cout << "Minimum element: " << min << endl;
+
+    return 0;
